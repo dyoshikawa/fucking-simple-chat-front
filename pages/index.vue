52d9be0cce7c62656f9af8c7bfa6124e7d232d5f
dyoshikawa <dyoshikawa1993@gmail.com>
2018-10-07 00:14:44 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,78 @@
 <template>
   <div>
-    <b-button @click="click">chat</b-button>
-
-    <b-pagination
-      size="md"
-      :total-rows="100"
-      :per-page="10"
-      @change="changePage($event)"
-    />
-
+    <h1>Fucking Simple Chat</h1>
     <ul>
-      <li
-        v-for="chat in computedChats"
-        :key="chat.id"
-      >
-        {{ chat.createdAt }} 
-        {{ chat.name }}
-        {{ chat.content }}
-      </li>
+      <li>Nuxt.js</li>
+      <li>express</li>
+      <li>socket.io</li>
+      <li>sequelize</li>
     </ul>
+
+    <div
+      class="mb-3"  
+    >
+      <b-form-group
+        label="Name"
+      >
+        <b-input
+          v-model="name"
+        />
+      </b-form-group>
+
+      <b-form-group
+        label="Message"
+      >
+        <b-input
+          v-model="content"
+        />
+      </b-form-group>
+
+      <b-button
+        @click="send"
+        variant="primary"
+      >
+        Send
+      </b-button>
+    </div>
+
+    <b-card
+      v-for="chat in computedChats"
+      :key="chat.id"
+      :title="chat.name"
+      class="mb-2"
+    >
+      {{ chat.content }}
+    </b-card>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Provide } from 'nuxt-property-decorator'
 import Logo from '~/components/Logo.vue'
 import { Chat } from '~/store/chats/types'
 import io from 'socket.io-client'
 
 const socket = io('http://localhost:8080')
 
-@Component({
-  components: {
-    Logo,
-  },
+@Component
+export default class extends Vue {
+  name: string = ''
+  content: string = ''
+
   async created() {
-    console.log('created')
     await this.$store.dispatch('chats/fetchChats')
     socket.on('chat message', chat => {
-      console.log(chat)
       this.$store.commit('chats/addChat', chat)
     })
-  },
-})
-export default class extends Vue {
+  }
+
   get computedChats(): Chat[] {
     return this.$store.getters['chats/chats']
   }
 
-  changePage(page: number) {
-    this.$store.dispatch('chats/fetchChats')
-  }
-
-  click() {
-    socket.emit('message', { name: 'name', content: 'content' })
+  send() {
+    socket.emit('message', { name: this.name, content: this.content })
+    this.content = ''
   }
 }
 </script>
